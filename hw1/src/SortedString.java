0e4fe4b07b2d3f905d59c01ca2af46ed29974262
Pedro Munoz <pmunoz@csumb.edu>
2017-11-14 21:42:20 -0800

    update

---
+++ hw1/src/SortedString.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class SortedString {
+    public static void main(String[] args) {
+        System.out.println("Input file name: ");
+        System.out.println("Output file name: ");
+        System.out.println("Done.");
+    }
+}
