7f7ad5a8953c0631e1ede25a2f010ea78b222903
Pedro Munoz <pmunoz@csumb.edu>
2017-11-14 22:45:12 -0800

    update

--- hw1/src/SortedString.java
+++ hw1/src/SortedString.java
@@ -1,9 +1,29 @@
 import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class SortedString {
+
     public static void main(String[] args) {
-        System.out.println("Input file name: ");
-        System.out.println("Output file name: ");
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Input file name: ");
+        String input = keyboard.next();
+
+        System.out.print("Output file name: ");
+        String fileOut = keyboard.next();
+
+
+        try {
+            Scanner fileIn = new Scanner(new FileInputStream(input));
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(0);
+        }
+
         System.out.println("Done.");
     }
 }
